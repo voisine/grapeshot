65550bfaeea57029a3789d0ebc7537fe0c8d9fab
Aaron Voisine <voisine@gmail.com>
2015-07-26 21:31:52 -0700

    xcode proj

---
+++ GrapeShot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrapeShot
+//
+//  Created by Aaron Voisine on 7/26/15.
+//  Copyright (c) 2015 Aaron Voisine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GrapeShotTests/GrapeShotTests.swift
@@ -0,0 +1,36 @@
+//
+//  GrapeShotTests.swift
+//  GrapeShotTests
+//
+//  Created by Aaron Voisine on 7/26/15.
+//  Copyright (c) 2015 Aaron Voisine. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GrapeShotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
